5207db6c45872ca37414b9d0c9d8e05023f6c086
Guilcher Pascal <pgu.dev@gmail.com>
2014-01-12 19:30:11 +0100

    test cors

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,15 @@
 'use strict';
 
 angular.module('pguGeoNgApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $http) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+
+    $http.get('http://pgu-geo-ng-api.appspot.com/tasks/').then(function(response) {
+      console.log(response);
+    });
+
+});
