7274461d6209e4f655fd7d35a2225254e943e41c
pgu <guilcher.p@sfeir.com>
2014-01-30 15:07:57 +0100

    add LinkedIn API call

--- app/scripts/controllers/loginCtrl.js
+++ app/scripts/controllers/loginCtrl.js
@@ -1,3 +1,10 @@
+'use strict';
+
 angular.module('pguGeoNgApp')
-    .controller('LoginCtrl', function () {
+    .controller('LoginCtrl', function ($scope, LinkedIn) {
+
+//        LinkedIn.get().then(function() {
+//            $scope.msg = 'IN is loaded';
+//        });
+
     });
--- app/scripts/services/linkedIn.js
+++ app/scripts/services/linkedIn.js
@@ -1,7 +1,39 @@
 'use strict';
 
-angular.module('pguGeoNgApp').factory('LinkedIn', function () {
-    return {
+angular.module('pguGeoNgApp').factory('LinkedIn', function ($q, $window) {
 
+    var IN = $window.IN;
+
+    return {
+        get: function() {
+
+            if (!_(IN).isUndefined()) {
+                console.log('direct IN');
+                return $q.when(IN);
+            }
+
+            console.log('load IN');
+            // loads the LinkedIn API
+            var deferred = $q.defer();
+            var promise = deferred.promise;
+
+            $window.$.ajax({
+                url: 'http://platform.linkedin.com/in.js',
+                dataType: 'script',
+                data: {
+                    api_key: 'qwfxh7u2673i',
+                    authorize: true,
+                    scope: 'r_fullprofile r_network'
+                }
+            })
+                .done(function () {
+                    deferred.resolve($window.IN);
+                })
+                .fail(function () {
+                    $window.alert('Ouch! Unable to load LinkedIn\'s API!');
+                });
+
+            return promise;
+        }
     };
 });
